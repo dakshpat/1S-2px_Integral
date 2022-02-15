4513d3418529050673b5a96fd0c930e92e8bdb98
Daksh <dpatel132@hawk.iit.edu>
2022-02-15 13:03:55 -0600

    Initial Commit

---
+++ Shared/_S_2px_IntegralApp.swift
@@ -0,0 +1,17 @@
+//
+//  _S_2px_IntegralApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct _S_2px_IntegralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
